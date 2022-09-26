241fdd50208083c9531b207db0a02af63fb4578a
TinsFox <fox@tinsfox.com>
2022-09-26 21:37:16 +0800

    chore: add postcss-px-to-viewport

---
+++ pages/welcome.tsx
@@ -0,0 +1,3 @@
+export default function () {
+  return <div>欢迎页</div>;
+}
--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,10 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    "postcss-px-to-viewport": {
+      viewportWidth: 375,
+      viewportUnit: "vw",
+      fontViewportUnit: "vw",
+    },
   },
-}
+};

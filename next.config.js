cdfca6b984e6d9f68f6eadbf686187565470f35c
TinsFox <fox@tinsfox.com>
2022-09-26 21:29:00 +0800

    chore: add react-vant and tailwindcss

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withImages = require("next-images");
 
-module.exports = nextConfig
+const withTM = require("next-transpile-modules")(["react-vant"]);
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withTM(
+  withImages({
+    // 你项目中其他的 Next.js 配置
+    reactStrictMode: true,
+    swcMinify: true,
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+  })
+);
+
+module.exports = nextConfig;

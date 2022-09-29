31038cacf5d675659e8328692c89f8ef095910fc
TinsFox <fox@tinsfox.com>
2022-09-29 10:13:01 +0800

    feat: welcome page

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -1,3 +1,13 @@
+import styles from "../styles/welcome.module.scss";
 export default function () {
-  return <div>欢迎页</div>;
+  return (
+    <>
+      <p className={styles.title}>欢迎来到生鲜！</p>
+      <div className={styles.description}>
+        无论您身在何处，我们都会为您提供您喜爱的食物，以满足您的食物需求，这里是一些文字描述
+      </div>
+      <button className={styles.getStarted}>现在开始</button>
+      <img src="/切图 1@2x.png" alt="" className={styles.img} />
+    </>
+  );
 }
